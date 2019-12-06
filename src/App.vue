81ea49454a8bf69367dca2d34bdd641727161628
Paul Luna <paulluna0215@gmail.com>
2019-12-05 17:47:30 -0800

    Initial commit (I think(

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <TodoList v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: false
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Completed Tasks: {{todos.filter(todo => { return todo.done === true}).length}}</p>
+        <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+        <Todo v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:key="index"></Todo>
+    </div>
+</template>
+
+<script>
+
+import Todo from './Todo'
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+</style>
