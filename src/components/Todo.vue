c182f138be68d63df515993b61735af788a534ca
Paul Luna <paulluna0215@gmail.com>
2019-12-08 21:21:25 -0800

    Can now mark todos as complete

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -33,10 +33,10 @@
                 </div>
             </div>
         </div>
-        <div class="ui bottom attached green basic button" v-show="!isEditing && todo.done" disabled>
+        <div class="ui bottom attached green basic button" v-on:click="completeTodo(todo)" v-show="!isEditing && todo.done" disabled>
             Completed
         </div>
-        <div class="ui bottom attached red basic button" v-show="!isEditing && !todo.done">
+        <div class="ui bottom attached red basic button" v-on:click="completeTodo(todo)" v-show="!isEditing && !todo.done">
             Pending
         </div>
     </div>
@@ -59,6 +59,9 @@ export default {
     },
     deleteTodo (todo) {
       this.$emit('delete-todo', todo)
+    },
+    completeTodo (todo) {
+      this.$emit('complete-todo', todo)
     }
   }
 }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,12 @@
     <div>
         <p>Completed Tasks: {{todos.filter(todo => { return todo.done === true}).length}}</p>
         <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-        <Todo v-on:delete-todo="deleteTodo" v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:key="index"></Todo>
+        <Todo
+          v-on:delete-todo="deleteTodo"
+          v-on:complete-todo="completeTodo"
+          v-for="(todo, index) in todos"
+          v-bind:todo="todo"
+          v-bind:key="index"></Todo>
     </div>
 </template>
 
@@ -19,6 +24,10 @@ export default {
     deleteTodo (todo) {
       const todoIndex = this.todos.indexOf(todo)
       this.todos.splice(todoIndex, 1)
+    },
+    completeTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos[todoIndex].done = !this.todos[todoIndex].done
     }
   }
 }
